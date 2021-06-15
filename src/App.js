40e1b2377ea9ff49535ab944860c9fd2263a0118
Adrian Sudbury <sudburya@gmail.com>
2021-06-15 19:18:40 +0100

    Remove Widgets from UI!

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Feed from "./components/Feed/Feed";
-import Widgets from "./components/Widgets/Widgets";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
       <div className="appBody">
         <Sidebar />
         <Feed />
-        <Widgets />
       </div>
     </div>
   );
